11cd0376fa33afd05152efb3e03c496256262be2
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2024-07-05 16:15:58 +0900

    docs(polyfill): add module document

--- src/polyfill.d.ts
+++ src/polyfill.d.ts
@@ -1,5 +1,9 @@
 // deno-lint-ignore-file no-var
+
+/** Return reference consisting of a component. */
 declare var compositeKey: (
   ...parts: [object, ...unknown[]] | [...unknown[], object]
 ) => Readonly<{ __proto__: null }>;
+
+/** Return symbol consisting of a components. */
 declare var compositeSymbol: (...parts: readonly unknown[]) => symbol;
--- src/polyfill.ts
+++ src/polyfill.ts
@@ -3,7 +3,8 @@
 
 /// <reference path="./polyfill.d.ts" />
 
-/**
+/** Polyfill affects the global object.
+ *
  * @example
  * ```ts
  * import "@miyauci/composite-key/polyfill";
